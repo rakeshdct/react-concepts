b59442e3d4e5d7b8932380630e578a2201ec2790
Rakesh <rakesh.v6@cognizant.com>
2023-01-06 14:41:54 +0530

    ProfileImageUpload feature development

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import ProfileImageUpload from './views/profileImageUpload'
+
+const profileInfo = {
+  "inital": "RV",
+  "name": "Profile Name",
+  "status": "Profile Status goes here... "
+}
 
 function App() {
   return (
     <div className="App">
-      APP
+      <ProfileImageUpload profileInfo={profileInfo} />
     </div>
   );
 }
---
+++ src/views/profileImageUpload.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import '../styles/profileImageUpload.css';
+
+const ProfileImageUpload = (props) => {
+    const [imagePreviewUrl, setimagePreviewUrl] = useState();
+    const photoUpload = e => {
+        e.preventDefault();
+        const reader = new FileReader();
+        if (e.target.files[0] !== undefined) {
+            const file = e.target.files[0];
+            reader.onloadend = () => {
+                setimagePreviewUrl(reader.result)
+            }
+            reader.readAsDataURL(file);
+        }
+    }
+    return (
+        <div className="page">
+            <div className="card">
+                <div>
+                    <h1>Profile Card</h1>
+                    <label htmlFor="photo-upload" className="custom-file-upload">
+                        <span>{props.profileInfo.inital}</span>
+                        <div className="img-wrap img-upload" >
+                            <span>Click here to upload image </span>
+                            <img htmlFor="photo-upload" src={imagePreviewUrl} alt="" />
+                        </div>
+                        <input id="photo-upload" type="file" accept=".jpg,.png" onChange={photoUpload} />
+                    </label>
+                    <h2>{props.profileInfo.name}</h2>
+                    <p>{props.profileInfo.status}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileImageUpload
